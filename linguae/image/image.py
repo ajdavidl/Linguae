50cd56839bef88c9e7cb5af0928aa3b0bdf6f900
Ajdavidl <ajdavidl@yahoo.com.br>
2022-08-31 14:19:34 -0300

    change url strings

--- linguae/image/image.py
+++ linguae/image/image.py
@@ -17,7 +17,7 @@ def googleImages(query):
         """
     if ' ' in query:
         query = re.sub(' ', '+', query)
-    url = 'https://www.google.com/search?tbm=isch&q=' + query
+    url = 'https://www.google.com/search?tbm=isch&q=%s' % query
     print(url)
     webbrowser.open_new_tab(url)
 
@@ -33,7 +33,6 @@ def duckduckGoImages(query):
         """
     if ' ' in query:
         query = re.sub(' ', '+', query)
-    url = 'https://duckduckgo.com/?q=' + query + \
-        '&t=ffab&iar=images&iax=images&ia=images'
+    url = 'https://duckduckgo.com/?q=%s&t=ffab&iar=images&iax=images&ia=images' % query
     print(url)
     webbrowser.open_new_tab(url)
