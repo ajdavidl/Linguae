d832bdb550945a8578f8ec385fc3dc9960a84bcd
Antonio David <78770941+ajdavidl@users.noreply.github.com>
2022-12-19 22:08:43 -0300

    fix docstring in concordance.py

--- linguae/concordance/concordance.py
+++ linguae/concordance/concordance.py
@@ -39,7 +39,7 @@ def concordance(listSentences, word):
      gracefully . Bill Clinton spoke in ambiguous language when asked to describe his relationship with 
      . It is important for you to learn a foreign language . I 'll open the curtain for you to look out 
     ifficult to understand a lecture in a foreign language . The students have taken no notice of these 
-     'll find it your advantage to know a foreign language .
+     'll find it your advantage to know a foreign language . It is almost impossible to learn a foreign 
 
     >>> PorSentences = linguae.loadLanguageTatoeba('por')
     >>> conc = linguae.concordance(PorSentences,'idioma')
@@ -49,7 +49,7 @@ def concordance(listSentences, word):
     eu cavalo . Que língua é falada no Egito ? Que idioma falam no Egito ? Que língua é utilizada no Egi
     rante três dias a partir da meia-noite . O meu idioma não está na lista ! Parte da ilha ficou devast
     sabe como ganhar dinheiro . O inglês não é meu idioma nativo . Inglês não é minha língua nativa . Bi
-    , eu fico . Ela está costurando um vestido . O idioma Na'vi é usado em Avatar . Vamos continuar com  It is almost impossible to learn a foreign 
+    , eu fico . Ela está costurando um vestido . O idioma Na'vi é usado em Avatar . Vamos continuar com 
     """
     if len(listSentences) > MAX_NR_SENTENCES:
         listSentences = listSentences[:MAX_NR_SENTENCES]
