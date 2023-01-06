08d54390a6d37fb16283c68b7423a52c5b0b42ce
Ajdavidl <ajdavidl@yahoo.com.br>
2023-01-06 07:33:09 -0300

    update docstring of textGeneration module

--- linguae/textGeneration/textGeneration.py
+++ linguae/textGeneration/textGeneration.py
@@ -6,120 +6,193 @@ from transformers import pipeline
 
 def loadBloom():
     """
-        Load Bloom Language model
+    Load Bloom Language model.
 
-        It uses the pipeline from transformers package.
+    It uses the pipeline from transformers package.
 
-        Parameters
-        ----------
-        No parameters
+    Parameters
+    ----------
+    No parameters.
 
-        Returns
-        -------
-        a Pipeline object from the transformers package with the Bloom model loaded
+    Returns
+    -------
+    a Pipeline object from the transformers package with the Bloom model loaded.
 
-        """
+    See Also
+    --------
+    loadmGPT : Load mGPT Language model.
+    loadGPTPortuguese : Load Portuguese GPT2 Language model.
+    loadGPTEnglish : Load English GPT2 Language model.
+    loadGPTSpanish : Load Spanish GPT2 Language model. 
+    generateText : Generate text using a language model.
+
+    Examples
+    --------
+    >>> pipelineBloom = linguae.loadBloom()
+    """
     return pipeline("text-generation", model="bigscience/bloom-1b1")
 
 
 def loadmGPT():
     """
-        Load mGPT Language model
+    Load mGPT Language model.
 
-        It uses the pipeline from transformers package.
+    It uses the pipeline from transformers package.
 
-        Parameters
-        ----------
-        No parameters
+    Parameters
+    ----------
+    No parameters.
 
-        Returns
-        -------
-        a Pipeline object from the transformers package with the mGPT model loaded
+    Returns
+    -------
+    a Pipeline object from the transformers package with the mGPT model loaded.
 
-        """
+    See Also
+    --------
+    loadBloom : Load Bloom Language model.
+    loadGPTPortuguese : Load Portuguese GPT2 Language model.
+    loadGPTEnglish : Load English GPT2 Language model.
+    loadGPTSpanish : Load Spanish GPT2 Language model. 
+    generateText : Generate text using a language model.
+
+    Examples
+    --------
+    >>> pipelinemGPT = linguae.loadmGPT()
+    """
     return pipeline("text-generation", model="sberbank-ai/mGPT")
 
 
 def loadGPTPortuguese():
     """
-        Load Portuguese GPT2 Language model
+    Load Portuguese GPT2 Language model.
 
-        It uses the pipeline from transformers package.
+    It uses the pipeline from transformers package.
 
-        Parameters
-        ----------
-        No parameters
+    Parameters
+    ----------
+    No parameters.
 
-        Returns
-        -------
-        a Pipeline object from the transformers package with the Portuguese GPT model loaded
+    Returns
+    -------
+    a Pipeline object from the transformers package with the Portuguese GPT model loaded.
 
-        """
+    See Also
+    --------
+    loadBloom : Load Bloom Language model.
+    loadmGPT : Load mGPT Language model.
+    loadGPTEnglish : Load English GPT2 Language model.
+    loadGPTSpanish : Load Spanish GPT2 Language model. 
+    generateText : Generate text using a language model.
+
+    Examples
+    --------
+    >>> pipelineGPTPor = linguae.loadGPTPortuguese()
+    """
     return pipeline("text-generation", model="pierreguillou/gpt2-small-portuguese")
 
 
 def loadGPTEnglish():
     """
-        Load English GPT2 Language model
+    Load English GPT2 Language model.
 
-        It uses the pipeline from transformers package.
+    It uses the pipeline from transformers package.
 
-        Parameters
-        ----------
-        No parameters
+    Parameters
+    ----------
+    No parameters.
 
-        Returns
-        -------
-        a Pipeline object from the transformers package with the Portuguese GPT model loaded
+    Returns
+    -------
+    a Pipeline object from the transformers package with the Portuguese GPT model loaded.
 
-        """
+    See Also
+    --------
+    loadBloom : Load Bloom Language model.
+    loadmGPT : Load mGPT Language model.
+    loadGPTPortuguese : Load Portuguese GPT2 Language model.
+    loadGPTSpanish : Load Spanish GPT2 Language model. 
+    generateText : Generate text using a language model.
+
+    Examples
+    --------
+    >>> pipelineGPTEng = linguae.loadGPTEnglish()
+    """
     return pipeline("text-generation", model="gpt2-medium")
 
 
 def loadGPTSpanish():
     """
-        Load Spanish GPT2 Language model
+    Load Spanish GPT2 Language model.
 
-        It uses the pipeline from transformers package.
+    It uses the pipeline from transformers package.
 
-        Parameters
-        ----------
-        No parameters
+    Parameters
+    ----------
+    No parameters.
 
-        Returns
-        -------
-        a Pipeline object from the transformers package with the Portuguese GPT model loaded
+    Returns
+    -------
+    a Pipeline object from the transformers package with the Portuguese GPT model loaded.
 
-        """
+    See Also
+    --------
+    loadBloom : Load Bloom Language model.
+    loadmGPT : Load mGPT Language model.
+    loadGPTPortuguese : Load Portuguese GPT2 Language model.
+    loadGPTEnglish : Load English GPT2 Language model.
+    generateText : Generate text using a language model.
+
+    Examples
+    --------
+    >>> pipelineGPTSpa = linguae.loadGPTSpanish()
+    """
     return pipeline("text-generation", model="datificate/gpt2-small-spanish")
 
 
 def generateText(modelPipeline, textSeed, textSize=80, numberSentences=1):
     """
-        It receives the pipeline with the language model loaded; 
-        It gives the text received as seed; 
-        and generate the next words.
+    Generate text using a language model.
 
-        It uses the transformers pipeline model to generate text.
+    It receives the pipeline with the language model loaded; 
+    It gives the text received as seed; 
+    and generate the next words.
 
-        Parameters
-        ----------
-        modelPipeline : pipeline from transformers package with the model loaded.
+    It uses the transformers pipeline model to generate text.
 
-        textSeed : str
-            The text to be used by the language model as a seed.
+    Parameters
+    ----------
+    modelPipeline : pipeline from transformers package with the model loaded.
 
-        textSize : integer
-            The number of words to be generated.
+    textSeed : str
+        The text to be used by the language model as a seed.
 
-        numberSentences : integer
-            number of sentences to be generated.
+    textSize : integer
+        The number of words to be generated.
 
-        Returns
-        -------
-        str
-            String with the seed text and all text generated.
-        """
+    numberSentences : integer
+        number of sentences to be generated.
+
+    Returns
+    -------
+    str
+        String with the seed text and all text generated.
+
+    See Also
+    --------
+    loadBloom : Load Bloom Language model.
+    loadmGPT : Load mGPT Language model.
+    loadGPTPortuguese : Load Portuguese GPT2 Language model.
+    loadGPTEnglish : Load English GPT2 Language model.
+    loadGPTSpanish : Load Spanish GPT2 Language model. 
+
+    Examples
+    --------
+    >>> pipelineGPTEng = linguae.loadGPTEnglish()
+    >>> linguae.generateText(pipelineGPTEng, "Learning languages is very important to ")
+
+    >>> pipelineBloom = linguae.loadBloom()
+    >>> linguae.generateText(pipelineBloom, "Learning languages is very important to ")
+    """
     result = modelPipeline(textSeed, max_length=textSize, temperature=0.8, top_p=0.92,
                            num_return_sequences=numberSentences)
     textOutput = []
