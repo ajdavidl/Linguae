a14eaff0e6a6bfffcd933138cf659598c54dc86b
Ajdavidl <ajdavidl@yahoo.com.br>
2023-02-03 17:34:56 -0300

    Update docstring in verb conjugation module.

--- linguae/verbConjugation/verbConjugation.py
+++ linguae/verbConjugation/verbConjugation.py
@@ -10,24 +10,34 @@ from verbecc import Conjugator
 
 def conjugation(language, verb):
     """
-        Conjugate a verb from given language.
+    Conjugate a verb from given language.
 
-        It uses the verbecc package under the hood.
+    It uses the verbecc package under the hood.
 
-        Parameters
-        ----------
-        language : str
-            Language of the text.
-            example: 'pt', 'es', 'fr', 'ro', 'it'
+    Parameters
+    ----------
+    language : str
+        Language of the text.
+        example: 'pt', 'es', 'fr', 'ro', 'it'
 
-        verb : str
-            verb to be conjugated.
+    verb : str
+        verb to be conjugated.
 
-        Returns
-        -------
-        str
-            String with the verb conjugation in json format.
-        """
+    Returns
+    -------
+    str
+        String with the verb conjugation in json format.
+
+    See Also
+    --------
+    linguae.verbix : Open browser and query the Verbix site.
+
+    Examples
+    --------
+    >>> linguae.conjugation('pt','aprender')
+    >>> linguae.conjugation('es','aprender')
+    >>> linguae.conjugation('it','imparare')
+    """
     if language not in ['pt', 'es', 'fr', 'it', 'ro']:
         print("Language not supported!")
         return None
@@ -38,18 +48,30 @@ def conjugation(language, verb):
 
 def verbix(language, verb):
     """
-        Open browser and query the Verbix site
+    Open browser and query the Verbix site
 
-        Parameters
-        ----------
-        language : str
-            Language of the text.
-            examples: 'portuguese', 'spanish', 'french', 'english', 'italian', 'german'
+    Parameters
+    ----------
+    language : str
+        Language of the text.
+        examples: 'portuguese', 'spanish', 'french', 'english', 'italian', 'german'
 
-        verb : str
-            verb to be conjugated
+    verb : str
+        verb to be conjugated
 
-        """
+    See Also
+    --------
+    linguae.conjugation : Conjugate a verb from given language.
+
+    Examples
+    --------
+    >>> linguae.verbix('english','learn')
+    https://www.verbix.com/webverbix/english/learn
+    >>> linguae.verbix('portuguese','aprender')
+    https://www.verbix.com/webverbix/portuguese/aprender
+    >>> linguae.verbix('spanish','aprender')
+    https://www.verbix.com/webverbix/spanish/aprender
+    """
     url = 'https://www.verbix.com/webverbix/%s/%s' % (language, verb)
     print(url)
     webbrowser.open_new_tab(url)
