79035710aba5bb12f433477570a8b6bb64627a6c
Ajdavidl <ajdavidl@yahoo.com.br>
2023-02-12 22:10:33 -0300

    Update docstring in word frequency module.

--- linguae/wordFrequency/wordFrequency.py
+++ linguae/wordFrequency/wordFrequency.py
@@ -3,25 +3,37 @@ Module to give the word frequency of a word.
 """
 from wordfreq import word_frequency
 
+
 def wordFreq(language, word):
     """
-        Get the frequency of `word` in the language with code `language`
+    Get the frequency of word in the language with code language.
 
-        It uses the wordfreq package under the hood.
+    It uses the wordfreq package under the hood.
 
-        Parameters
-        ----------
-        language : str
-            Language of the word.
-            example: 'en', 'pt', 'es', 'fr', 'de', 'ro', 'it'
+    Parameters
+    ----------
+    language : str
+        Language of the word.
+        example: 'en', 'pt', 'es', 'fr', 'de', 'ro', 'it'
 
-        word : str
-            word to be queried.
+    word : str
+        word to be queried.
 
-        Returns
-        -------
-        str
-            String with with the number of frequency.
-        """
+    Returns
+    -------
+    str
+        String with with the number of frequency.
+
+    Examples
+    --------
+    >>> linguae.wordFreq('en', 'the')
+    '0.0537'
+    >>> linguae.wordFreq('en', 'language')
+    '0.000126'
+    >>> linguae.wordFreq('pt', 'de')
+    '0.0479'
+    >>> linguae.wordFreq('pt', 'idioma')
+    '1.95e-05'
+    """
     freq = word_frequency(word=word, lang=language)
     return str(freq)
