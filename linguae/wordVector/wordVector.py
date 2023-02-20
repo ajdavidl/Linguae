4e9889d2a1278d6f76b71960d25332ec7c8db348
Ajdavidl <ajdavidl@yahoo.com.br>
2023-02-20 16:40:31 -0300

    Update docstring in word vector module.

--- linguae/wordVector/wordVector.py
+++ linguae/wordVector/wordVector.py
@@ -12,21 +12,34 @@ from .. import data
 
 def loadWordVectors(language):
     """
-        Load word vectors from the desired language
+    Load word vectors from the desired language.
 
-        It uses the MUSE pre-trained word embeddings and gensim KeyedVectors model.
+    It uses the MUSE pre-trained word embeddings and gensim KeyedVectors model.
 
-        Parameters
-        ----------
-        language : str
-            Language of the word.
-            example: 'en', 'pt', 'es', 'fr', 'de', 'ro', 'it'
+    Parameters
+    ----------
+    language : str
+        Language of the word.
+        example: 'en', 'pt', 'es', 'fr', 'de', 'ro', 'it'
 
-        Returns
-        -------
-        gensim KeyedVectors model
-            A gensim model with the word vectors loaded.
-        """
+    Returns
+    -------
+    gensim KeyedVectors model
+        A gensim model with the word vectors loaded.
+
+    See Also
+    --------
+    linguae.similarWords : Get similar words with word embeddings.
+    linguae.loadSentenceTransformerModel : Load a SentenceTransformer model.
+    linguae.encodeSentence : Computes sentence embeddings using sentence_transformers package.
+    linguae.similarSentences : Gives the most similar sentences from a query sentence using the sentence transformer model.
+
+    Examples
+    --------
+    >>> engVectors = linguae.loadWordVectors('en')
+    >>> porVectors = linguae.loadWordVectors('pt')
+
+    """
     vectorFile = 'wiki.multi.%s.vec' % language
     template = pkg_resources.open_text(data, vectorFile)
 
@@ -35,29 +48,46 @@ def loadWordVectors(language):
 
 def similarWords(fromLanguageVectors, word, toLanguagesVectors=[]):
     """
-        It receives the loaded word vectors model from one language; 
-        Then load a vector from a given word; 
-        and then gives the most similar words in each model given.
+    Get similar words with word embeddings.
 
-        It uses the gensim KeyedVectors model.
+    It receives the loaded word vectors model from one language; 
+    Then load a vector from a given word; 
+    and then gives the most similar words in each model given.
 
-        Parameters
-        ----------
-        fromLanguageVectors : gensim KeyedVectors model
-            The model load with the function loadWordVectors.
-            Alternatively, the model given by the funtion gensim.models.KeyedVectors.load_word2vec_format.
+    It uses the gensim KeyedVectors model.
 
-        word : str
-            word to be queried.
+    Parameters
+    ----------
+    fromLanguageVectors : gensim KeyedVectors model
+        The model load with the function loadWordVectors.
+        Alternatively, the model given by the funtion gensim.models.KeyedVectors.load_word2vec_format.
 
-        toLanguagesVectors : list of gensim KeyedVectors models
-            MUSE models from other languages loaded by the function loadWordVectors.
+    word : str
+        word to be queried.
 
-        Returns
-        -------
-        str
-            String with the similar words.
-        """
+    toLanguagesVectors : list of gensim KeyedVectors models
+        MUSE models from other languages loaded by the function loadWordVectors.
+
+    Returns
+    -------
+    str
+        String with the similar words.
+
+    See Also
+    --------
+    linguae.loadWordVectors : Load word vectors from the desired language.
+    linguae.loadSentenceTransformerModel : Load a SentenceTransformer model.
+    linguae.encodeSentence : Computes sentence embeddings using sentence_transformers package.
+    linguae.similarSentences : Gives the most similar sentences from a query sentence using the sentence transformer model.
+
+    Examples
+    --------
+    >>> engVectors = linguae.loadWordVectors('en')
+    >>> porVectors = linguae.loadWordVectors('pt')
+    >>> spaVectors = linguae.loadWordVectors('es')
+    >>> print(linguae.similarWords(engVectors, 'language', [porVectors, spaVectors]))
+    >>> print(linguae.similarWords(porVectors, 'idioma', [engVectors, spaVectors]))
+    """
     vector = fromLanguageVectors.get_vector(word)
     tuples = fromLanguageVectors.similar_by_vector(vector)
     textOutput = "%s:\n" % word
