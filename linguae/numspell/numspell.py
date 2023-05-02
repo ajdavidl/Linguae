2a0dc32c931956f8e23e145aa0bb9e1d4beccd9d
Ajdavidl <78770941+ajdavidl@users.noreply.github.com>
2023-05-02 12:14:02 -0300

    Change main function in numspell module to be able to support more languages.

--- linguae/numspell/numspell.py
+++ linguae/numspell/numspell.py
@@ -3,7 +3,14 @@ Module that convert integer numbers in its spelling.
 """
 
 
-def num2words(num):
+def num2words(lang, num):
+    if lang == 'en':
+        return __num2wordsEng(num)
+    else:
+        return "Sorry, language not supported."
+
+
+def __num2wordsEng(num):
     ones = ['', 'one', 'two', 'three', 'four',
             'five', 'six', 'seven', 'eight', 'nine']
     tens = ['', '', 'twenty', 'thirty', 'forty',
@@ -13,7 +20,7 @@ def num2words(num):
     if num == 0:
         return 'zero'
     elif num < 0:
-        return 'minus ' + num2words(abs(num))
+        return 'minus ' + __num2wordsEng(abs(num))
     else:
         if num < 10:
             return ones[num]
@@ -22,8 +29,8 @@ def num2words(num):
         elif num < 100:
             return tens[num // 10] + ' ' + ones[num % 10]
         elif num < 1000:
-            return ones[num // 100] + ' hundred ' + num2words(num % 100)
+            return ones[num // 100] + ' hundred ' + __num2wordsEng(num % 100)
         elif num < 1000000:
-            return num2words(num // 1000) + ' thousand ' + num2words(num % 1000)
+            return __num2wordsEng(num // 1000) + ' thousand ' + __num2wordsEng(num % 1000)
         else:
             return 'number out of range'
