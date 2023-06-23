a07d3e9fb40d9dcc341708d09ff6cf3aa874f62c
Ajdavidl <78770941+ajdavidl@users.noreply.github.com>
2023-06-23 17:50:13 -0300

    Add sentence Report function and change word Report function.

--- linguae/report/report.py
+++ linguae/report/report.py
@@ -29,6 +29,9 @@ def wordReport(language, word):
         print("Language not supported!")
         return
 
+    print()
+    print(f"Report of the word: {word}")
+    print()
     print(f"Definition of the word {word} in wiktionary:")
     print(obj.wiktionary(word))
     print()
@@ -40,6 +43,25 @@ def wordReport(language, word):
     print()
     print(f"Word Frequency in {obj.name} language:")
     print(obj.wordFreq(word))
+    print()
+    if language == "en":
+        print("Translation to Portuguese")
+        print(obj.translateTo("pt", word))
+        print()
+        print("Translation to Spanish")
+        print(obj.translateTo("es", word))
+    elif language == "pt":
+        print("Translation to English")
+        print(obj.translateTo("en", word))
+        print()
+        print("Translation to Spanish")
+        print(obj.translateTo("es", word))
+    else:
+        print("Translation to English")
+        print(obj.translateTo("en", word))
+        print()
+        print("Translation to Portuguese")
+        print(obj.translateTo("pt", word))
     print()
     print(f"Similar Words in {obj.name} language:")
     print(obj.similarWords(word))
@@ -55,5 +77,87 @@ def wordReport(language, word):
     print(f"Text samples of the word {word} in {language} language:")
     print(obj.textSamples(word, 25))
     print()
-    print(f"Now listen to the sound of word {word} in {language} Portuguese!")
+    print(f"Now listen to the sound of word {word} in {language} language!")
     obj.tts(word)
+
+
+def sentenceReport(language, sentence):
+
+    if language == "en":
+        obj = English()
+    elif language == "pt":
+        obj = Portuguese()
+    elif language == "es":
+        obj = Spanish()
+    elif language == "it":
+        obj = Italian()
+    elif language == "fr":
+        obj = French()
+    elif language == "de":
+        obj = German()
+    else:
+        print("Language not supported!")
+        return
+
+    print()
+    print(f"Report of the sentence: {sentence}")
+    print()
+    if language == "en":
+        print("Translation to Spanish")
+        print(obj.translateTo("es", sentence))
+        print()
+        print("Translation to Portuguese")
+        print(obj.translateTo("pt", sentence))
+        print()
+        print("Translation to French")
+        print(obj.translateTo("fr", sentence))
+        print()
+        print("Translation to German")
+        print(obj.translateTo("de", sentence))
+    elif language == "pt":
+        print("Translation to English")
+        print(obj.translateTo("en", sentence))
+        print()
+        print("Translation to Spanish")
+        print(obj.translateTo("es", sentence))
+        print()
+        print("Translation to French")
+        print(obj.translateTo("fr", sentence))
+        print()
+        print("Translation to Italian")
+        print(obj.translateTo("it", sentence))
+        print()
+        print("Translation to German")
+        print(obj.translateTo("de", sentence))
+    elif language == "es":
+        print("Translation to English")
+        print(obj.translateTo("en", sentence))
+        print()
+        print("Translation to Portuguese")
+        print(obj.translateTo("pt", sentence))
+    else:
+        print("Translation to English")
+        print(obj.translateTo("en", sentence))
+        print()
+        print("Translation to Portuguese")
+        print(obj.translateTo("pt", sentence))
+        print()
+        print("Translation to Spanish")
+        print(obj.translateTo("es", sentence))
+    print()
+    print("Parsing the sentence:")
+    print(obj.parse(sentence))
+    print()
+    obj.loadTatoebaList()
+    obj.trimTatoebaList(50000)
+    print("Similar Sentences on Tatoeba")
+    print(obj.similarSentences(sentence, 20))
+    print()
+    print("Entities in the sentence:")
+    print(obj.nerSpacy(sentence))
+    print()
+    print("Entity link")
+    print(obj.dbpediaEntityLink(sentence))
+    print()
+    print(f"Now listen to the sound of sentence in {language} language!")
+    obj.tts(sentence)
