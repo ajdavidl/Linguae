06a0bd52a7e9d9ed09640c90e73e000e93d52a9a
Ajdavidl <78770941+ajdavidl@users.noreply.github.com>
2025-02-28 23:11:18 -0300

    Add function frequency to module wordFrequency.

--- linguae/wordFrequency/Readme.md
+++ linguae/wordFrequency/Readme.md
@@ -2,15 +2,18 @@
 
 Module to give the frequency of a word.
 
-It uses the [wordfreq](https://github.com/rspeer/wordfreq) package under the hood.
+It uses the [wordfreq](https://github.com/rspeer/wordfreq) package in the function `wordFreq`. And it uses the sklearn `CountVectorizer` method in the function `frequency`.
 
-Main function:
+Functions:
 
 ```
 linguae.wordFreq : Get the frequency of word in a given language.
+linguae.frequency : Calculate the frequency os tokens (unigrams, bigrams ...) of a list of strings.
 ```
 
 Examples:
+
+* wordFreq
 
 ```python
 >>> import linguae
@@ -22,4 +25,13 @@ Examples:
 '0.0479'
 >>> linguae.wordFreq('pt', 'idioma')
 '1.95e-05'
+```
+
+* frequency
+
+```python
+>>> listEng = linguae.loadLanguageTatoeba('eng')
+>>> listEngFreq = linguae.frequency(listEng)
+>>> listEngFreq[:10]
+[('to', 496283), ('the', 472956), ('tom', 447763), ('that', 314474), ('you', 282733), ('is', 219346), ('he', 186283), ('in', 170506), ('and', 164284), ('of', 163454)]
 ```
--- linguae/wordFrequency/wordFrequency.py
+++ linguae/wordFrequency/wordFrequency.py
@@ -2,7 +2,7 @@
 Module to give the frequency of a word.
 """
 from wordfreq import word_frequency
-
+from sklearn.feature_extraction.text import CountVectorizer
 
 def wordFreq(language, word):
     """
@@ -17,7 +17,7 @@ def wordFreq(language, word):
         example: 'en', 'pt', 'es', 'fr', 'de', 'ro', 'it'
 
     word : str
-        word to be queried.
+        Word to be queried.
 
     Returns
     -------
@@ -37,3 +37,47 @@ def wordFreq(language, word):
     """
     freq = word_frequency(word=word, lang=language)
     return str(freq)
+
+
+
+def frequency(listText, stopwords=None, ngramRange=(1, 1), vocabulary=None):
+    """
+    Calculate the frequency os tokens (unigrams, bigrams ...) of a list of strings.
+
+    Parameters
+    ----------
+    listText : list of str
+        List with the texts.
+    
+    stopwords : list of str
+        List of the stop words.
+    
+    ngramRange : tuple (min_n, max_n), default=(1, 1)
+        The number of tokens in the ngram. For example an ngramRange of (1, 1) means only unigrams, (1, 2) means unigrams and bigrams, and (2, 2) means only bigrams.
+    
+    vocabulary : list of str
+        List of the words/ngrams to be added to the frequency list.
+
+    Returns
+    -------
+    List of tuples
+
+    Examples
+    --------
+    >>> listEng = linguae.loadLanguageTatoeba('eng')
+    >>> listEngFreq = linguae.frequency(listEng)
+    >>> listEngFreq[:10]
+    """
+    count_vect = CountVectorizer(
+        analyzer='word',
+        stop_words=stopwords,
+        ngram_range=ngramRange,
+        vocabulary=vocabulary
+    )
+    count_vect.fit(listText)
+    bag_of_words = count_vect.transform(listText)
+    sum_words = bag_of_words.sum(axis=0)
+    word_freq = [(word, sum_words[0, idx])
+                 for word, idx in count_vect.vocabulary_.items()]
+    word_freq = sorted(word_freq, key=lambda x: x[1], reverse=True)
+    return word_freq
